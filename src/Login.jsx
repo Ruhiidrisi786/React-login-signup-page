c0208a64580c5462b5ffb778f4ab9b50d666ff36
Ruhiidrisi786 <rukaiyabano888@gmail.com>
2025-09-29 11:25:52 +0530

    update file

--- src/Login.jsx
+++ src/Login.jsx
@@ -11,6 +11,28 @@ function Login(){
                         <button className={isLogin ? "active" : ""} onClick={() => setIsLogin(true)} >Login</button>
                         <button className={!isLogin ? "active" : ""} onClick={() => setIsLogin(false)}>Signup</button>
                     </div>
+                
+            
+            {isLogin ? <>
+                <div>
+                    <h2>Login Form</h2>
+                    <input type="text" placeholder="Email Address" />
+                    <input type="password" placeholder="Password" />
+                    <p className="forgot-password">Forgot password?</p>
+                    <button className="login-btn">Login</button>
+                    <p className="bottom">Not a member? <a href="#" onClick={() => setIsLogin(false)}>Signup now</a></p>
+                </div>
+            </> : 
+                <>
+                <div>
+                    <h2>Signup Form</h2>
+                    <input className="signup-input" type="text" placeholder="Email Address"/>
+                    <input className="signup-input" type="password" placeholder="Password" />
+                    <input className="signup-input" type="password"  placeholder="Confirm password"/>
+                    <button className="signup-btn">Signup</button>
+                </div>
+                </>
+            }
                 </div>
             </div>
         </>
